76c6a4d2e5a90b5ea10c131f786bee3a774c7a20
cdfzo <cdfzo@pm.me>
2024-02-18 17:27:28 +0100

    Fix linter

--- tests/utils/server.test.ts
+++ tests/utils/server.test.ts
@@ -3,7 +3,7 @@ import { App } from '@cdfzo/kh'
 
 describe('#listen', () => {
   it('starts the server', async () => {
-    const app = new App().listen(3000)
+    new App().listen(3000)
 
     expect((await fetch('http://127.0.0.1:3000')).ok).toBe(true)
   })
